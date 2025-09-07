b72ca82892806afa0117a0a35acd313ab9ae4760
Alex Cook <a5cook@uwaterloo.ca>
2025-09-07 11:24:53 -0400

    chore(spotless): apply spotless

--- framework/src/main/java/io/github/eisop/runtimeframework/agent/RuntimeAgent.java
+++ framework/src/main/java/io/github/eisop/runtimeframework/agent/RuntimeAgent.java
@@ -1,9 +1,7 @@
 package io.github.eisop.runtimeframework.agent;
 
-import java.lang.instrument.Instrumentation;
-
 public class RuntimeAgent {
-    public static void premain(String args, java.lang.instrument.Instrumentation inst) {
-	System.out.println("Agent Initialized");
-    }
+  public static void premain(String args, java.lang.instrument.Instrumentation inst) {
+    System.out.println("Agent Initialized");
+  }
 }
