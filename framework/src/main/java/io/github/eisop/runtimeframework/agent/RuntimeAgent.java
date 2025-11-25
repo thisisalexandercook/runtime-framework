3d26575c4f80084697d8742c9ace6f843f1c99db
Alex Cook <a5cook@uwaterloo.ca>
2025-11-25 14:17:13 -0500

    refactor(framework.agent): update agent to use debugging checker

--- framework/src/main/java/io/github/eisop/runtimeframework/agent/RuntimeAgent.java
+++ framework/src/main/java/io/github/eisop/runtimeframework/agent/RuntimeAgent.java
@@ -1,12 +1,15 @@
 package io.github.eisop.runtimeframework.agent;
 
+import io.github.eisop.runtimeframework.core.RuntimeChecker;
 import io.github.eisop.runtimeframework.filter.FrameworkSafetyFilter;
+import io.github.eisop.runtimeframework.util.SysOutRuntimeChecker;
 import java.lang.instrument.Instrumentation;
 
 public final class RuntimeAgent {
 
   public static void premain(String args, Instrumentation inst) {
     var filter = new FrameworkSafetyFilter();
-    inst.addTransformer(new RuntimeTransformer(filter), false);
+    RuntimeChecker checker = new SysOutRuntimeChecker();
+    inst.addTransformer(new RuntimeTransformer(filter, checker), false);
   }
 }
