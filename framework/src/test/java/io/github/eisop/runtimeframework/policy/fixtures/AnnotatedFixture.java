ed8bcb4eb7bebef4e2c4f90f199ee41c66fbfbf7
Alex Cook <a5cook@uwaterloo.ca>
2026-04-16 13:24:34 -0400

    test: remove old tests

--- framework/src/test/java/io/github/eisop/runtimeframework/policy/fixtures/AnnotatedFixture.java
+++
@@ -1,6 +0,0 @@
-package io.github.eisop.runtimeframework.policy.fixtures;
-
-import io.github.eisop.runtimeframework.qual.AnnotatedFor;
-
-@AnnotatedFor("nullness")
-public class AnnotatedFixture {}
