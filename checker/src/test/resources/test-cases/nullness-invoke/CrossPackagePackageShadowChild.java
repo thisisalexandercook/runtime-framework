4ad2d43052dda74415237cda18c8067fff3ba934
Alex Cook <a5cook@uwaterloo.ca>
2026-05-11 11:25:44 -0400

    test: check for package-private shadowing

--- checker/src/test/resources/test-cases/nullness-invoke/CrossPackagePackageShadowChild.java
+++ checker/src/test/resources/test-cases/nullness-invoke/CrossPackagePackageShadowChild.java
@@ -6,5 +6,6 @@ import io.github.eisop.runtimeframework.qual.AnnotatedFor;
 @AnnotatedFor("nullness")
 public class CrossPackagePackageShadowChild extends CheckedAccessBase {
     void packageAccept(Object value) {
+        throw new AssertionError("package-private shadow should not override base package method");
     }
 }
